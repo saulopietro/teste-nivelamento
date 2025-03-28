b04eb2dffe28cf5be2ffb0167bd37ade079d2502
saulopietro <saulo154@outlook.com>
2025-03-27 22:51:59 -0300

    PdfDownloadServiceTest

---
+++ web-scraping/src/test/java/com/test_web_scraping/web_scraping/service/PdfDownloadServiceTest.java
@@ -0,0 +1,51 @@
+package com.test_web_scraping.web_scraping.service;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PdfDownloadServiceTest {
+
+    private PdfDownloadService pdfDownloadService;
+
+    @BeforeEach
+    void setUp() {
+        pdfDownloadService = new PdfDownloadService();
+    }
+
+    @Test
+    void TheFilesShouldBeDonwloaded() throws IOException {
+
+        //Arrange
+        String downloadDir = "C:\\Users\\saulo\\Desktop\\teste1";
+        List<String> fileUrls = new ArrayList<>();
+        List<String> filePaths = new ArrayList<>();
+
+        fileUrls.add("https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos/Anexo_I_Rol_2021RN_465.2021_RN627L.2024.pdf");
+        fileUrls.add("https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos/Anexo_II_DUT_2021_RN_465.2021_RN628.2025_RN629.2025.pdf");
+        //Act
+        for (String fileUrl : fileUrls) {
+            URL url = new URL(fileUrl);
+
+            String fileName = fileUrl.substring(fileUrl.lastIndexOf("/") + 1);
+
+            File file = new File(downloadDir + "/" + fileName);
+            FileUtils.copyURLToFile(url, file);
+
+            System.out.println("Downloaded files: " + file.getAbsolutePath());
+            filePaths.add(file.getAbsolutePath());
+        }
+
+        //Assert
+        assertFalse(filePaths.isEmpty());
+    }
+}
--- web-scraping/src/test/java/com/test_web_scraping/web_scraping/service/PdfScraperServiceTest.java
+++ web-scraping/src/test/java/com/test_web_scraping/web_scraping/service/PdfScraperServiceTest.java
@@ -1,26 +1,59 @@
 package com.test_web_scraping.web_scraping.service;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.junit.Assert;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PdfScraperServiceTest {
 
+    @Autowired
+    private PdfScraperService pdfScraperService;
+
     @Test
-    void scrapePdfLinks() throws IOException {
-        PdfScraperService pdfScraperService = new PdfScraperService();
+    void ShouldFindPdfLink() throws IOException {
 
-        List<String> links = pdfScraperService.scrapePdfLinks("https://www.gov.br/ans/pt-br/acesso-a-informacao/informacoes-classificadas-e-desclassificadas");
-
-        assertNotNull(links);
-        assertTrue(links.size() > 0);
-        assertTrue(links.get(0).contains("pdf"));
-        assertTrue(links.get(0).contains("Anexo"));
-
+        //Arrange
+        String url = "https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos";
+        List<String> links = new ArrayList<>();
+        //Act
+        Document doc = Jsoup.connect(url).timeout(10000).get();
+        Elements pdfLinks = doc.select("a[href$='.pdf']");
+        for(Element link: pdfLinks) {
+            links.add(link.absUrl("href"));
+        }
+        //Assert
+        assertFalse(links.isEmpty());
     }
 
-
+    @Test
+    void ShouldFindTwoPdfWithNameAnexo() throws IOException {
+        //Arrange
+        String url = "https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos";
+        List<String> links = new ArrayList<>();
+        //Act
+        Document doc = Jsoup.connect(url).timeout(10000).get();
+        Elements pdfLinks = doc.select("a[href*='Anexo'][href$='.pdf']");
+        for(Element link: pdfLinks) {
+            links.add(link.absUrl("href"));
+        }
+        //Assert
+        assertEquals(2, links.size());
+    }
 }
