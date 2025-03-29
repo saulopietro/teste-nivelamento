ff13a1c0284b95672ef6726431842828a3b86660
saulopietro <saulo154@outlook.com>
2025-03-29 13:33:44 -0300

    queries update

---
+++ scripts_sql/sql/import_scripts/import_last_3_months_demonstracoes.sql
@@ -0,0 +1,14 @@
+COPY demonstracoes_contabeis(data, reg_ans, cd_conta_contabil, descricao, vl_saldo_inicial, vl_saldo_final)
+FROM 'C:\projetos\teste_nivelamento\scripts_sql\data\demonstracoes_contabeis_2024\4T2024.csv'
+DELIMITER ';'          
+CSV HEADER;
+
+-- Alterar a coluna vl_saldo_inicial para DECIMAL(17, 2)
+ALTER TABLE demonstracoes_contabeis
+    ALTER COLUMN vl_saldo_inicial TYPE DECIMAL(17, 2)
+    USING REPLACE(vl_saldo_inicial, ',', '.')::DECIMAL(17, 2);
+
+-- Alterar a coluna vl_saldo_final para DECIMAL(17, 2)
+ALTER TABLE demonstracoes_contabeis
+    ALTER COLUMN vl_saldo_final TYPE DECIMAL(17, 2)
+    USING REPLACE(vl_saldo_final, ',', '.')::DECIMAL(17, 2);
