6568e7fc4fe87b5f474cabe7fc8c01802ff3479d
saulopietro <saulo154@outlook.com>
2025-03-29 13:42:08 -0300

    add ENCODING 'UTF8';

--- scripts_sql/sql/import_scripts/import_last_3_months_demonstracoes.sql
+++ scripts_sql/sql/import_scripts/import_last_3_months_demonstracoes.sql
@@ -1,7 +1,8 @@
 COPY demonstracoes_contabeis(data, reg_ans, cd_conta_contabil, descricao, vl_saldo_inicial, vl_saldo_final)
 FROM 'C:\projetos\teste_nivelamento\scripts_sql\data\demonstracoes_contabeis_2024\4T2024.csv'
 DELIMITER ';'          
-CSV HEADER;
+CSV HEADER
+ENCODING 'UTF8';     
 
 -- Alterar a coluna vl_saldo_inicial para DECIMAL(17, 2)
 ALTER TABLE demonstracoes_contabeis
--- scripts_sql/sql/import_scripts/import_operadoras.sql
+++ scripts_sql/sql/import_scripts/import_operadoras.sql
@@ -1,4 +1,5 @@
 COPY operadoras_plano_saude(registro_ans, cnpj, razao_social, nome_fantasia, modalidade, logradouro, numero, complemento, bairro, cidade, uf, cep, ddd, telefone, fax, endereco_eletronico, representante, cargo_representante, regiao_de_comercializacao, data_registro_ans)
 FROM 'C:\projetos\teste_nivelamento\scripts_sql\data\operadoras_ativas_ans\Relatorio_cadop.csv'
 DELIMITER ';'   
-CSV HEADER;      
+CSV HEADER
+ENCODING 'UTF8';      
