401b747c5f9603700a80dc3e5fc9a765fb309610
saulopietro <saulo154@outlook.com>
2025-03-29 13:24:55 -0300

    queries + imports

---
+++ scripts_sql/sql/analytics_queries/query_last_1_year.sql
@@ -0,0 +1,10 @@
+select cnpj, razao_social, 
+SUM(CAST(demonstracoes_contabeis.vl_saldo_final AS DECIMAL)) AS total_despesas
+from operadoras_plano_saude
+INNER JOIN demonstracoes_contabeis ON demonstracoes_contabeis.reg_ans = operadoras_plano_saude.registro_ans
+WHERE descricao = 'EVENTOS/ SINISTROS CONHECIDOS OU AVISADOS  DE ASSISTÊNCIA A SAÚDE MEDICO HOSPITALAR '
+GROUP BY
+    operadoras_plano_saude.razao_social, operadoras_plano_saude.cnpj
+ORDER BY
+    total_despesas DESC
+LIMIT 10
--- scripts_sql/sql/tb_create_scripts/create_demonstracoes.sql
+++ scripts_sql/sql/tb_create_scripts/create_demonstracoes.sql
@@ -1,8 +1,8 @@
 CREATE TABLE demonstracoes_contabeis (   
-    reg_ans SERIAL PRIMARY KEY,            
+    reg_ans BIGINT,            
     cd_conta_contabil VARCHAR(255) NOT NULL, 
     descricao VARCHAR(255),                  
-    vl_saldo_inicial DECIMAL(15, 2),         
-    vl_saldo_final DECIMAL(15, 2),           
+    vl_saldo_inicial VARCHAR(17),         
+    vl_saldo_final VARCHAR(17),           
     data DATE                      
-);
+)
