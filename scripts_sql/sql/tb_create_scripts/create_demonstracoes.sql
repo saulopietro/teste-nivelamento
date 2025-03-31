cdfe89d85c8ca6afc1fbe08a93814fc2d6f32313
saulopietro <saulo154@outlook.com>
2025-03-31 11:32:02 -0300

    update queries

--- scripts_sql/sql/tb_create_scripts/create_demonstracoes.sql
+++ scripts_sql/sql/tb_create_scripts/create_demonstracoes.sql
@@ -1,8 +1,9 @@
-CREATE TABLE demonstracoes_contabeis (   
+CREATE TABLE demonstracoes_contabeis (
+  	id SERIAL PRIMARY KEY,
     reg_ans BIGINT,            
     cd_conta_contabil VARCHAR(255) NOT NULL, 
     descricao VARCHAR(255),                  
     vl_saldo_inicial VARCHAR(17),         
     vl_saldo_final VARCHAR(17),           
-    data DATE                      
+    data DATE
 )
